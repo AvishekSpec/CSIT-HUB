601f17fc97e5339d40da4433e5e194313bc3f7e7
Avishek <70626424+AvishekSpec@users.noreply.github.com>
2022-02-17 16:44:05 +0545

    Add files via upload

---
+++ NavBar/Discussion.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Discussion extends StatelessWidget {
+  const Discussion({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text(
+        'Coming Soon !',
+        style: TextStyle(fontSize: 20),
+      ),
+    ));
+  }
+}
---
+++ NavBar/events.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Events extends StatelessWidget {
+  const Events({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text(
+        'Coming Soon !',
+        style: TextStyle(fontSize: 20),
+      ),
+    ));
+  }
+}
